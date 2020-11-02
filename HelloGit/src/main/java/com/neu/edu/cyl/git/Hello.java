baebcfc071034c048d407f75f099e7b3bf99da5f
cuiyongling <2290483862@qq.com>
2020-11-02 21:36:34 +0800

    主干添加

--- HelloGit/src/main/java/com/neu/edu/cyl/git/Hello.java
+++ HelloGit/src/main/java/com/neu/edu/cyl/git/Hello.java
@@ -10,6 +10,7 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello Git!");
         System.out.println("添加分支!");
+        System.out.println("主干添加！");
 
     }
 }
