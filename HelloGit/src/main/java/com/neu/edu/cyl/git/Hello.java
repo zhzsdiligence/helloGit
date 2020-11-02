e4574b094750186ab40e75ff3285df694571a0bb
cuiyongling <2290483862@qq.com>
2020-11-02 21:13:30 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/neu/edu/cyl/git/Hello.java
@@ -0,0 +1,13 @@
+package com.neu.edu.cyl.git;
+
+/**
+ * @author cuiyongling
+ * @since V1.0.0
+ * 2020-11-02 20:59
+ */
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Git!");
+    }
+}
