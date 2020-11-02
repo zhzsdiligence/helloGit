a81fac55506889d5dcd8ffe36b06baad4c25255a
cuiyongling <2290483862@qq.com>
2020-11-02 22:19:27 +0800

    测试

--- HelloGit/src/main/java/com/neu/edu/cyl/git/Hello.java
+++ HelloGit/src/main/java/com/neu/edu/cyl/git/Hello.java
@@ -11,7 +11,7 @@ public class Hello {
         System.out.println("Hello Git!");
         System.out.println("添加分支!");
         System.out.println("主干添加！");
-
         System.out.println("分支添加！");
+        System.out.println("测试");
     }
 }
